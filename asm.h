aabb155e96267f3eaf79763ca24ce831ee42f3b5
Yuting Wang <yuting.wang@sjtu.edu.cn>
2025-04-02 21:02:08 +0800

    Comment SEG_ASM

--- asm.h
+++ asm.h
@@ -8,6 +8,25 @@
 
 // The 0xC0 means the limit is in 4096-byte units
 // and (for executable segments) 32-bit mode.
+// 
+// ------------------------------------------------------------
+// Comment by: Yuting Wang
+// Date: Wed Apr  2 12:47:58 PM UTC 2025
+// ------------------------------------------------------------
+// 1. This marcro creates an GDT entry (64-bit), starting from
+//    lower bits to higher ones.
+// 2. lim is 32-bit limit with the page granularity. Therefore,
+//    (lim >> 12) should be the limit stored in a GDT entry.
+//    (limt >> 12) is split into lower 16-bits and higher 8-bits.
+// 3. base is 32-bit and split into severl places
+// 4. type is 4-bit and specifies the type of this entry
+// 5. 0x90 indicates that
+//       P = 1, entry is present
+//       DPL = 0, requires ring 0 privilige to access
+//       S = 1, this is a code/data segment
+// 6. 0xC0 indicates that
+//       G = 1, page granuality
+//       D/B = 1, default operand size is 32-bit (a 32-bit segment)
 #define SEG_ASM(type,base,lim)                                  \
         .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);      \
         .byte (((base) >> 16) & 0xff), (0x90 | (type)),         \
